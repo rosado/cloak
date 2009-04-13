9e23787249801bb6f195056bb8aad457e8204bd6
Roland Sadowski <szabla@gmail.com>
2009-04-13 19:17:16 +0200

    Fix whitespace.

--- rosado/cloak/actions.clj
+++ rosado/cloak/actions.clj
@@ -7,7 +7,7 @@
 ;; which can be found in the file CPL.TXT at the root of this
 ;; distribution.  By using this software in any fashion, you are
 ;; agreeing to be bound by the terms of this license.  You must not
-;; remove this notice, or any other, from this software. 
+;; remove this notice, or any other, from this software.
 
 
 (ns rosado.cloak.actions
@@ -18,41 +18,42 @@
 (defn sh
   "Performs a shell command."
   ([command]
-	 (run-command command))
+     (run-command command))
   ([command flag]
-	 (cond (= flag :dofail) (when (= :fail (sh command))
-							  (throw (Exception. "shell command failed.")))
-		   :else (throw (IllegalArgumentException. (format "sh: unsupported flag %s" (str flag)))))))
+     (cond (= flag :dofail)
+             (when (= :fail (sh command))
+               (throw (Exception. "shell command failed.")))
+           :else (throw (IllegalArgumentException. (format "sh: unsupported flag %s" (str flag)))))))
 
 (defn copy
   "Copies a file"
   [src target]
   (let [src (File. src) t (File. target)
-		target (if (.isDirectory t)
-				 (File. target (.getName src))
-				 t)]
-	(when-not (.exists src)
-	  (throw (java.io.FileNotFoundException. (format "copy: Source file not found: %s" src))))
-	(try
-	 (let [in (FileInputStream. src)
-		   out (FileOutputStream. target)
-		   buff (make-array Byte/TYPE 4096)]
-	   (loop [nbytes (.read in buff)]
-		 (when (not= nbytes -1)
-		   (.write out buff 0 nbytes)
-		   (recur (.read in buff)))))
-	 (finally
-	  (try	   
- 	   (.close src)
- 	   (.close target)
-	   (catch Exception e nil))))
-	:ok))
+        target (if (.isDirectory t)
+                 (File. target (.getName src))
+                 t)]
+    (when-not (.exists src)
+      (throw (java.io.FileNotFoundException. (format "copy: Source file not found: %s" src))))
+    (try
+     (let [in (FileInputStream. src)
+           out (FileOutputStream. target)
+           buff (make-array Byte/TYPE 4096)]
+       (loop [nbytes (.read in buff)]
+         (when (not= nbytes -1)
+           (.write out buff 0 nbytes)
+           (recur (.read in buff)))))
+     (finally
+      (try
+       (.close src)
+       (.close target)
+       (catch Exception e nil))))
+    :ok))
 
 (defn move
   "Moves a file"
   [src target]
   (let [src (File. src) target (File. target)]
-	(.renameTo src target)))
+    (.renameTo src target)))
 
 (defn mkdir
   "Creates directories, including necessary parent dirs."
